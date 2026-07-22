1e57e1403b8f6b8315572206cccaf4c11c9cba8c
Sayuri Zúñiga Chacón <233349@ids.upchiapas.edu.mx>
2026-07-21 19:53:54 -0600

    Fix API URL for web production

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 class ApiConstants {
   static String get baseUrl {
-    if (kIsWeb) return 'http://localhost:3000/api/v1';
+    if (kIsWeb) return 'https://farmacare-api.onrender.com/api/v1';
     return 'http://10.0.2.2:3000/api/v1';
   }
 }
